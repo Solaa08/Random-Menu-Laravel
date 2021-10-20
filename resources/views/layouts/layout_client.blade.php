4adac18375609a315476baa8ff41f6a8a54b6923
mael <mael.jamin@gmail.com>
2021-10-20 17:38:09 +0200

    MAJ css pour https

--- resources/views/layouts/layout_client.blade.php
+++ resources/views/layouts/layout_client.blade.php
@@ -9,13 +9,13 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Tutoriel Laravel 8 CRUD</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('css/cover.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ secure_asset('css/cover.css') }}" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.11.2/r-2.2.9/datatables.min.css"/>
     <!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.2/css/dataTables.bootstrap4.min.css"> -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.11.2/r-2.2.9/datatables.min.js"></script>
-    <script src="{{ asset('js/app.js') }}" type="text/js" defer></script>
+    <script src="{{ secure_asset('js/app.js') }}" type="text/js" defer></script>
 </head>
 
 <body>
